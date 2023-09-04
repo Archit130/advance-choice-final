f457df7e3220357907434e5ab476cf1005cede09
Archit130 <architdeora1999@gmail.com>
2023-09-04 12:44:24 +0530

    up

--- js/index.js
+++ js/index.js
@@ -45,3 +45,10 @@ btn.onclick = function () {
     }
   }, 20);
 };
+
+$(document).ready(function () {
+  // Fakes the loading setting a timeout
+  setTimeout(function () {
+    $("body").addClass("loaded");
+  }, 3500);
+});
