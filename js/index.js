d35f28308103673ca4376c6ca9c28e33888d0c6b
Archit130 <architdeora1999@gmail.com>
2023-09-04 13:08:33 +0530

    who we are

--- js/index.js
+++ js/index.js
@@ -52,3 +52,17 @@ $(document).ready(function () {
     $("body").addClass("loaded");
   }, 3500);
 });
+
+// const tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ".trigger",
+//     start: "top top",
+//     end: "+=1000",
+//     scrub: 1,
+//     pin: true,
+//     markers: true,
+//   },
+// });
+// tl.to(".box", { yPercent: 350, duration: 1 });
+// tl.to(".box", { rotation: 360, duration: 3 });
+// tl.to(".box", { xPercent: 350, duration: 1 });
